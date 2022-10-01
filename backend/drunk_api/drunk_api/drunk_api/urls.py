f59065133b0340c85c44ff90fdb9dcafe3b68d22
Calvin Liu <calvin.liu68@gmail.com>
2022-10-01 17:59:23 -0400

    api setup

--- backend/drunk_api/drunk_api/drunk_api/urls.py
+++ backend/drunk_api/drunk_api/drunk_api/urls.py
@@ -1,4 +1,4 @@
-"""drunkdetection URL Configuration
+"""drunk_api URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,9 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from drunk_api.drunkapp import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+router.register(r'results', views.ResultsViewSet)
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- backend/drunkdetection/README.md
+++
@@ -1,6 +0,0 @@
-To run the server: 
-```sh
-python manage.py runserver
-```
-
-It should be running at http://127.0.0.1:8000/
