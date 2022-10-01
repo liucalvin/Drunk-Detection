80fd6aebf0e137ddcd90022579279a484c0d7a86
YeeticusFinch <50182007+YeeticusFinch@users.noreply.github.com>
2022-10-01 19:18:12 -0400

    kfold cross_val_score is a bitch

--- ml/FancyDrunkAnalyzerModelTrainer.py
+++ ml/FancyDrunkAnalyzerModelTrainer.py
@@ -6,7 +6,16 @@ import sys
 import json
 import numpy as np
 import os
+import pandas as pd
 import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from scikeras.wrappers import KerasClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import StratifiedKFold
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
 #import keras.datasets as keras_data
 
 #from sklearn.datasets import load_breast_cancer
@@ -48,10 +57,7 @@ drunk_table = []
 def format_row(set):
     result = {}
     for type in types:
-        #print(set['faces'][0])
         for metric in set['faces'][0][type]:
-            #print(set['faces'][0][type][metric])
-            #print(type(set['faces'][0][type][metric]))
             if isinstance(set['faces'][0][type][metric], dict):
                 for v in set['faces'][0][type][metric]:
                     if isinstance(set['faces'][0][type][metric][v], dict):
@@ -61,7 +67,6 @@ def format_row(set):
                     else:
                         #print(type + '_' + metric + '_' + v + " = " + str(set['faces'][0][type][metric][v]))
                         result[type + '_' + metric + '_' + v] = set['faces'][0][type][metric][v]
-            #result[type + '_' + metric]
     return result
 
 # sober_set = [{person 1}, {person 2}, {person 3}]
@@ -72,8 +77,9 @@ for set in drunk_set:
     
 for set in sober_set:
     sober_table.append(format_row(set))
-    
-#print(drunk_table)
+
+#print(drunk_table)   
+#print(sober_table)
 
 #print(drunk_set[0]['faces'][0]['landmark'])
 
@@ -97,10 +103,34 @@ for i in range(0, len(sober_table) + len(drunk_table)):
             y.append(0)
             si+=1
 
-
-
 #x, y = load_breast_cancer(return_X_y=True)
-#x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+
+# x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=0)
+
+# Baseline Model
+def create_baseline():
+    print("Creating Baseline Model")
+    model = Sequential()
+    model.add(Dense(len(sober_table[0]), input_shape=(len(sober_table[0]),), activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
+    # Compile Model
+    print("Compiling Baseline Model")
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    print("Compiled Baseline Model")
+    return model
+
+# evaluate model with standardized dataset
+def evaluate_model():
+    print("Evaluating Baseline Model")
+    estimator = KerasClassifier(model=create_baseline(), epochs=100, batch_size=5, verbose=0)
+    print("Created Estimator")
+    kfold = StratifiedKFold(n_splits=10, shuffle=True)
+    print("Created kfold")
+    results = cross_val_score(estimator, x, y, cv=kfold)
+    print("Created results")
+    print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+
+evaluate_model()
 
 # check otu a sample
 #print(x_train.shape)
