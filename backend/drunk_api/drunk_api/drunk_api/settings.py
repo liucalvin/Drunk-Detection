bf6e6358c0b5f4b999b7337d7404c673196915dc
Calvin Liu <calvin.liu68@gmail.com>
2022-10-02 09:40:50 -0400

    this sucks

--- backend/drunk_api/drunk_api/drunk_api/settings.py
+++ backend/drunk_api/drunk_api/drunk_api/settings.py
@@ -31,11 +31,6 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "127.0.0.1",
-    "drunk-detector.herokuapp.com"
-]
-
 
 # Application definition
 
