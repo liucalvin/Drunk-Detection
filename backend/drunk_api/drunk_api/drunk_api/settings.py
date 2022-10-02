8220881b6120dbc6c7c0d831ff450fba62d53067
Calvin Liu <calvin.liu68@gmail.com>
2022-10-02 01:39:17 -0400

    heroku config please work

--- backend/drunk_api/drunk_api/drunk_api/settings.py
+++ backend/drunk_api/drunk_api/drunk_api/settings.py
@@ -31,7 +31,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "drunk-detector.herokuapp.com"
+]
 
 
 # Application definition
