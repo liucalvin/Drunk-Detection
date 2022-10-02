82bb43bf7db0b1982cd910b13bddeadf18acf62c
Calvin Liu <calvin.liu68@gmail.com>
2022-10-01 20:48:59 -0400

    finish frontend form submit

--- frontend/drunk-detection/src/App.js
+++ frontend/drunk-detection/src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import {
+  Center,
+  Box,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Text,
+  Badge,
+  Flex,
+} from "@chakra-ui/react";
 
 function App() {
+  const [input, setInput] = useState();
+  const [response, setResponse] = useState();
+
+  const submitForm = async () => {
+    const formData = new FormData();
+    formData.append("file", input);
+
+    try {
+      console.log(process.env.REACT_APP_DRUNK_API_URL);
+      const response = await fetch(process.env.REACT_APP_DRUNK_API_URL, {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response) {
+        console.log(response.body);
+        const data = await response.json();
+        setResponse(data);
+      }
+    } catch (error) {
+      console.log("error!!!");
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Center h="100vh">
+        {response ? (
+          <Text>{response.toString()}</Text>
+        ) : (
+          <Box p="10" maxW="520px" borderWidth="1px">
+            <Flex align="baseline" mt={2}>
+              <Badge colorScheme="purple">ATTENTION</Badge>
+              <Text
+                ml={2}
+                textTransform="uppercase"
+                fontSize="sm"
+                fontWeight="bold"
+                color="purple.700"
+              >
+                Are you drunk???
+              </Text>
+            </Flex>
+            <FormControl>
+              <FormLabel>
+                Add your drunk/sober photo here to find out NOW!!!
+              </FormLabel>
+              <Input
+                borderWidth="0px"
+                type="file"
+                onChange={(e) => {
+                  console.log(e);
+                  console.log(e.target.files[0]);
+                  setInput(e.target.files[0]);
+                }}
+              />
+              <FormHelperText>Your data is safe with us ;&#41;</FormHelperText>
+            </FormControl>
+            <Button
+              mt="24px"
+              onClick={() => {
+                submitForm();
+              }}
+            >
+              Submit
+            </Button>
+          </Box>
+        )}
+      </Center>
+    </ChakraProvider>
   );
 }
 
