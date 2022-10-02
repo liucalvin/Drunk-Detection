ec893c028c94335220a13ca44c6179e17cc14781
Calvin Liu <calvin.liu68@gmail.com>
2022-10-02 02:55:26 -0400

    athena's fixes

--- frontend/drunk-detection/src/App.js
+++ frontend/drunk-detection/src/App.js
@@ -16,6 +16,7 @@ import {
 function App() {
   const [input, setInput] = useState();
   const [response, setResponse] = useState();
+  const [results, setResults] = useState();
 
   const submitForm = async () => {
     const formData = new FormData();
@@ -37,7 +38,12 @@ function App() {
       if (response) {
         console.log(response);
         const data = await response.json();
-        console.log(data.success);
+        const parsed = JSON.parse(data.success);
+        // console.log(parsed);
+        // console.log(parsed.faces);
+        // console.log(parsed.faces[0]);
+        // console.log(parsed.faces[0].attributes);
+        setResults(parsed.faces[0].attributes);
         setResponse(data.success);
         // const data = await response.body.getReader();
         // console.log(data);
@@ -51,12 +57,17 @@ function App() {
   return (
     <ChakraProvider>
       <Center h="100vh">
-        {response ? (
+        {response && results ? (
           <>
             <Flex align="baseline" mt={2} flexDirection="column">
-              <Text maxW="500px" pt="24px" maxH="400px">
-                {response.toString()}
+              <Text pt="24px">Age: {results.age.value}</Text>
+              <Text pt="24px">
+                Female Beauty Score: {results.beauty.female_score}
               </Text>
+              <Text pt="24px">
+                Male Beauty Score: {results.beauty.male_score}
+              </Text>
+              <Text pt="24px">Gender: {results.gender.value}</Text>
               <Button
                 mt="24px"
                 onClick={() => {
@@ -68,47 +79,48 @@ function App() {
             </Flex>
           </>
         ) : (
-          <Image 
-          src="../public/IMG_0305.jpg"
-          maxW="100%"
-          />
-          <Box p="10" maxW="520px" borderWidth="1px">
-            <Flex align="baseline" mt={2}>
-              <Badge colorScheme="purple">ATTENTION</Badge>
-              <Text
-                ml={2}
-                textTransform="uppercase"
-                fontSize="sm"
-                fontWeight="bold"
-                color="purple.700"
-              >
-                Are you drunk???
-              </Text>
-            </Flex>
-            <FormControl>
-              <FormLabel>
-                Add your drunk/sober photo here to find out NOW!!!
-              </FormLabel>
-              <Input
-                borderWidth="0px"
-                type="file"
-                onChange={(e) => {
-                  console.log(e);
-                  console.log(e.target.files[0]);
-                  setInput(e.target.files[0]);
+          <>
+            <Image src="../public/IMG_0305.jpg" maxW="100%" />
+            <Box p="10" maxW="520px" borderWidth="1px">
+              <Flex align="baseline" mt={2}>
+                <Badge colorScheme="purple">ATTENTION</Badge>
+                <Text
+                  ml={2}
+                  textTransform="uppercase"
+                  fontSize="sm"
+                  fontWeight="bold"
+                  color="purple.700"
+                >
+                  Are you drunk???
+                </Text>
+              </Flex>
+              <FormControl>
+                <FormLabel>
+                  Add your drunk/sober photo here to find out NOW!!!
+                </FormLabel>
+                <Input
+                  borderWidth="0px"
+                  type="file"
+                  onChange={(e) => {
+                    console.log(e);
+                    console.log(e.target.files[0]);
+                    setInput(e.target.files[0]);
+                  }}
+                />
+                <FormHelperText>
+                  Your data is safe with us ;&#41;
+                </FormHelperText>
+              </FormControl>
+              <Button
+                mt="24px"
+                onClick={() => {
+                  submitForm();
                 }}
-              />
-              <FormHelperText>Your data is safe with us ;&#41;</FormHelperText>
-            </FormControl>
-            <Button
-              mt="24px"
-              onClick={() => {
-                submitForm();
-              }}
-            >
-              Submit
-            </Button>
-          </Box>
+              >
+                Submit
+              </Button>
+            </Box>
+          </>
         )}
       </Center>
     </ChakraProvider>
